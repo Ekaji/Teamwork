91d7f689b59de368b0031ac2d6b8193d26c6468d
ekaji <ekajionah@gmail.com>
2019-11-14 12:42:01 +0100

    connected to postgres

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+const db = require('./queries');
 
-app.use((req, res) => {
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+app.get('/', (req, res) => {
     res.json({message: 'this is a json response'})
 })
 
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+
 module.exports = app;
+
